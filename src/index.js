073ba7cabdddda9b3d741cb1608b8e23321df379
elvinciqueira <elvinciqueira@gmail.com>
2020-01-13 10:52:26 -0200

    tooltip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import Tooltip from './ninja-ui/tooltip'
 
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
           presets: ['@babel/preset-env']
         }
       }
-    }]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+    ]
   }
 };
