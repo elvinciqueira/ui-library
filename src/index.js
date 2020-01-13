d3de4978a0a6ddc1a9e8621b72ec48e5c786aef7
elvinciqueira <elvinciqueira@gmail.com>
2020-01-13 11:05:34 -0200

    dropdowns

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import Tooltip from './ninja-ui/tooltip'
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+//create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instace = new Dropdown(dropdown);
+  instace.init();
+});
+
+
